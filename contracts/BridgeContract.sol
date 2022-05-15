96581945ebb8e481a0773e1c1906af5c986ef2ae
zerephe <zerephe97@gmail.com>
2022-05-15 17:53:32 +0600

    include exclude token issu fixed

--- contracts/BridgeContract.sol
+++ contracts/BridgeContract.sol
@@ -9,19 +9,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract TokenBridge is Ownable {
     event swapInitialized(address from, address to, address tokenAddr, uint256 amount, uint256 nonce);
 
-    ISwapToken private swapToken;
-
     uint256 public chainId;
     uint256 private nonce = 0;
+
+    address private nullAddress = 0x0000000000000000000000000000000000000000;
 
     mapping(address => mapping(uint256 => bool)) private isNonced;
     mapping(uint256 => bool) public supportedChainId;
     mapping(uint256 => bytes32) private processedSignature;
-    mapping(address => bool) public supportedToken;
+    mapping(address => ISwapToken) public supportedToken;
 
     constructor(address _erc20, uint _chainId) {
-        swapToken = ISwapToken(_erc20);
-        supportedToken[_erc20] = true;
+        supportedToken[_erc20] = ISwapToken(_erc20);
         chainId = _chainId;
     }
 
@@ -33,12 +32,12 @@ contract TokenBridge is Ownable {
     }
 
     function swap(address to, address tokenAddr, uint256 amount, uint256 _chainId) external returns(bool){
-        require(supportedToken[tokenAddr], "Token not supported!");
+        require(address(supportedToken[tokenAddr]) != nullAddress, "Token not supported!");
         require(supportedChainId[_chainId], "Blockchain doesnt supported");
         require(!isNonced[msg.sender][nonce], "Swap was already processed!");
 
         isNonced[msg.sender][nonce] = true;
-        swapToken.burnFrom(msg.sender, amount);
+        supportedToken[tokenAddr].burnFrom(msg.sender, amount);
 
         emit swapInitialized(msg.sender, to, tokenAddr, amount, nonce);
         
@@ -55,7 +54,7 @@ contract TokenBridge is Ownable {
             uint256 _nonce,
             uint8 v, bytes32 r, bytes32 s
         ) public returns (bool){
-        require(supportedToken[tokenAddr], "Token not supported!");
+        require(address(supportedToken[tokenAddr]) != nullAddress, "Token not supported!");
         bytes32 message = keccak256(abi.encodePacked(from, tokenAddr, amount, nonce));
         bytes32 hashedMessage = hashMessage(message);
         address addr = ecrecover(hashedMessage, v, r, s);
@@ -64,7 +63,7 @@ contract TokenBridge is Ownable {
         require(addr == from, "Invalid transaction!");
 
         processedSignature[_nonce] = hashedMessage;
-        swapToken.mint(to, amount);
+        supportedToken[tokenAddr].mint(to, amount);
 
         return true;
     }
@@ -75,12 +74,12 @@ contract TokenBridge is Ownable {
     }
 
     function includeToken(address tokenAddr) external onlyOwner returns(bool){
-        supportedToken[tokenAddr] = true;
+        supportedToken[tokenAddr] = ISwapToken(tokenAddr);
         return true;
     }
 
     function excludeToken(address tokenAddr) external onlyOwner returns(bool){
-        supportedToken[tokenAddr] = false;
+        supportedToken[tokenAddr] = ISwapToken(nullAddress);
         return true;
     }
 
